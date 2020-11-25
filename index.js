7f51ce25a25eb08c9fa5da39118b982dadfe22ca
lisongyu <269876940@qq.com>
2020-11-25 23:29:14 +0800

    提交文件添加

---
+++ index.js
@@ -0,0 +1,34 @@
+var Koa = require('koa');
+ var router = require('koa-router')();
+
+
+var url = require('url');
+const serve = require('koa-static');
+const IO = require('koa-socket')
+const io = new IO();
+
+var app = new Koa();
+app.use(serve('static'))
+io.attach(app);
+
+
+
+router.get('/', async (ctx) => {
+    await ctx.render('index');
+})
+
+app.use(router.routes());   /*启动路由*/
+app.use(router.allowedMethods());
+
+app._io.on('connection', socket => {
+    console.log('建立连接了');
+
+    // console.log(groupid);
+    // 监听客户端连接
+    socket.on("addData",function(data){
+        console.log(data);
+        socket.broadcast.emit("addInputData",data);
+    })
+})
+
+app.listen(3000);
